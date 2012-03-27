fd944584978fde73b728a36d5bc052c13cbe6404
Guido Cossu <cossu@post.kek.jp>
2012-03-27 14:05:08 +0900

    Testing multishift solver with 5d domain wall fermions

--- tests/test_MultiShiftSolver.hpp
+++ tests/test_MultiShiftSolver.hpp
@@ -12,14 +12,19 @@
 
 class Test_MultiShiftSolver : public TestGeneral{
  private:
+  const char* test_name;
   XML::node MS_node;
-  GaugeField& Gauge;
+  GaugeField Gauge;
   
   int test1();
  public:
-  Test_MultiShiftSolver(XML::node node,
-			GaugeField& conf):MS_node(node),
-					  Gauge(conf){}
+  Test_MultiShiftSolver(XML::node node, GaugeField& conf):MS_node(node),
+							  Gauge(conf)
+  {
+    test_name = "MultiShift";
+    XML::descend(MS_node, test_name);    
+  }
+
   int run();
 };
 
--- tests/testerMultiShiftSolver.cpp
+++ tests/testerMultiShiftSolver.cpp
@@ -8,33 +8,12 @@
 //------------------------------------------------------------------------
 
 #include "test_MultiShiftSolver.hpp"
-#include "include/commandline.hpp"
-
-using namespace XML;
-using namespace MapsEnv;
 
 int main(int argc, char* argv[]){
 
-  CommandOptions Options = ReadCmdLine(argc, argv);
-  
-  //Reading input file
-  node top_node = getInputXML(Options.filename);  
-
-  //Initializing geometry using XML input
-  Geometry geom(top_node);
-  initialize_mapper();
-
-  //Initialize GaugeField using XML input
-  GaugeGlobal GaugeF(geom);
-  GaugeF.initialize(top_node);
-  /////////////
-  
-  node MS_node = top_node;
-  descend(MS_node, "MultiShift");
-  
-  
-  Test_MultiShiftSolver MSTest(MS_node,GaugeF);
-  MSTest.run();
+  CREATE_TEST(Test_MultiShiftSolver);
+  RUN_TEST;
+  CLEAR_TEST;  
 
   return 0;
 }
