1e0331ba5bf713ba87a4d919b35828ae62fc4bac
Guido Cossu <cossu@post.kek.jp>
2012-04-02 16:03:25 +0900

    Modifications to support BGQ environment

--- lib/Communicator/communicator_mpi.cpp
+++ lib/Communicator/communicator_mpi.cpp
@@ -16,6 +16,8 @@ int Communicator::ipe_[Ndim_max_];
 int Communicator::nd_up_[Ndim_max_];
 int Communicator::nd_dn_[Ndim_max_];
 
+#ifndef IBM_BGQ
+
 Communicator* Communicator::instance(){
   static Communicator instance_;
   return &instance_;
@@ -246,3 +248,251 @@ int Communicator::pprintf(const char* format ...)const{
 }
 
 
+#endif
+
+
+#ifdef IBM_BGQ
+
+static double* pTmpBuffer = NULL;
+static int TmpBufferSize = 0;
+
+Communicator* Communicator::instance(){
+  static Communicator instance_;
+  return &instance_;
+}
+
+void Communicator::setup(){
+  BGNET_Init();
+  my_rank_ = BGNET_Rank();
+  Nproc_ = BGNET_Procs();
+
+  int nodeid = my_rank_;
+
+  int NPEx = CommonPrms::instance()->NPEx();
+  int NPEy = CommonPrms::instance()->NPEy();
+  int NPEz = CommonPrms::instance()->NPEz();
+  int NPEt = CommonPrms::instance()->NPEt();
+
+  CCIO::cout.init(&std::cout);
+  CCIO::cerr.init(&std::cerr);
+
+  //Check number of nodes
+  if (NPEx*NPEy*NPEz*NPEt != Nproc_) {
+    if(my_rank_==0) 
+      cerr << "Number of nodes provided is different from MPI environment ["
+	   << Nproc_<<"]\n"; 
+    abort();
+  }
+
+  int px =  nodeid %NPEx;
+  int py = (nodeid/NPEx) %NPEy;
+  int pz = (nodeid/(NPEx*NPEy)) %NPEz;
+  int pt = (nodeid/(NPEx*NPEy*NPEz)) %NPEt;
+
+  ipe_[0] = px;
+  ipe_[1] = py;
+  ipe_[2] = pz;
+  ipe_[3] = pt;
+
+  nd_up_[0] = ((px+1)%NPEx) +py*NPEx +pz*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_up_[1] = px +((py+1)%NPEy)*NPEx +pz*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_up_[2] = px +py*NPEx +((pz+1)%NPEz)*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_up_[3] = px +py*NPEx +pz*NPEx*NPEy +((pt+1)%NPEt)*NPEx*NPEy*NPEz;
+
+  nd_dn_[0] = ((px-1+NPEx)%NPEx) +py*NPEx +pz*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_dn_[1] = px +((py-1+NPEy)%NPEy)*NPEx +pz*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_dn_[2] = px +py*NPEx +((pz-1+NPEz)%NPEz)*NPEx*NPEy +pt*NPEx*NPEy*NPEz;
+  nd_dn_[3] = px +py*NPEx +pz*NPEx*NPEy +((pt-1+NPEt)%NPEt)*NPEx*NPEy*NPEz;
+
+  if(my_rank_==0) cout << "Communicator initialized using BGNET with "
+		       << Nproc_ << " processes.\n";
+}
+
+Communicator::~Communicator(){  BGNET_Finalize();}
+
+bool Communicator::primaryNode(){
+  if (my_rank_==0) return true;
+  return false;
+}
+
+//bool Communicator::primaryNode(){ return (my_rank_? true : false);}
+
+int Communicator::nodeid(int x, int y, int z, int t)const{
+  int NPEx = CommonPrms::instance()->NPEx();
+  int NPEy = CommonPrms::instance()->NPEy();
+  int NPEz = CommonPrms::instance()->NPEz();
+  int NPEt = CommonPrms::instance()->NPEt();
+  return x +NPEx*(y +NPEy*(z +NPEz*t));
+}
+
+void Communicator::
+transfer_fw(double *bin,double *data,int size,int dir) const{
+  int p_send = nd_dn_[dir];
+  int p_recv = nd_up_[dir];
+
+  int tag1 = dir*Nproc_+my_rank_;
+  int tag2 = dir*Nproc_+p_recv;
+
+  BGNET_Sendrecv(0,data,size*sizeof(double),p_send,bin,size*sizeof(double),p_recv);
+}
+
+void Communicator::
+transfer_fw(valarray<double>& bin,const valarray<double>& data,int dir) const{
+  transfer_fw(&bin[0],&(const_cast<valarray<double>& >(data))[0],
+	      bin.size(),dir);
+}
+
+void Communicator::
+transfer_fw(valarray<double>& bin,const valarray<double>& data,
+	    const vector<int>& index, int dir) const
+{
+  double* pTmp;
+  double* pIn;
+  int i,size;
+  int p_send = nd_dn_[dir];
+  int p_recv = nd_up_[dir];
+  int tag1 = dir*Nproc_+my_rank_;
+  int tag2 = dir*Nproc_+p_recv;
+
+  size = index.size();
+
+  if(size > TmpBufferSize){
+    if(pTmpBuffer != NULL){
+      free(pTmpBuffer);
+    }
+    pTmpBuffer = (double*)malloc(size*sizeof(double));
+    TmpBufferSize = size;
+  }
+
+  pIn = &(const_cast<valarray<double>& >(data))[0];
+  pTmp = pTmpBuffer;
+  for(i=0;i<size;i++){
+    pTmp[i] = pIn[index[i]];
+  }
+
+  BGNET_Sendrecv(0,pTmp,size*sizeof(double),p_send,&(const_cast<valarray<double>& >(bin))[0],size*sizeof(double),p_recv);
+}
+
+void Communicator::
+transfer_bk(double *bin,double *data,int size,int dir) const{
+  int p_send = nd_up_[dir];
+  int p_recv = nd_dn_[dir];
+  int Ndim = CommonPrms::instance()->Ndim();
+  int tag1 = (Ndim +dir)*Nproc_+my_rank_;
+  int tag2 = (Ndim +dir)*Nproc_+p_recv;
+
+  BGNET_Sendrecv(0,data,size*sizeof(double),p_send,bin,size*sizeof(double),p_recv);
+}
+
+void Communicator::
+transfer_bk(valarray<double>& bin,const valarray<double>& data,int dir) const{
+  transfer_bk(&(bin[0]),&(const_cast<valarray<double>& >(data))[0],
+	      bin.size(),dir);
+}
+
+void Communicator::
+transfer_bk(valarray<double>& bin,const valarray<double>& data,
+	    const vector<int>& index, int dir) const
+{
+  double* pTmp;
+  double* pIn;
+  int i,size;
+  int p_send = nd_up_[dir];
+  int p_recv = nd_dn_[dir];
+
+  size = index.size();
+
+  if(size > TmpBufferSize){
+    if(pTmpBuffer != NULL){
+      free(pTmpBuffer);
+    }
+    pTmpBuffer = (double*)malloc(size*sizeof(double));
+    TmpBufferSize = size;
+  }
+
+  pIn = &(const_cast<valarray<double>& >(data))[0];
+  pTmp = pTmpBuffer;
+  for(i=0;i<size;i++){
+    pTmp[i] = pIn[index[i]];
+  }
+
+  BGNET_Sendrecv(0,pTmp,size*sizeof(double),p_send,&(const_cast<valarray<double>& >(bin))[0],size*sizeof(double),p_recv);
+}
+
+void Communicator::send_1to1(double *bin,double *data,int size,
+			     int p_to,int p_from,int tag) const{
+  if(p_to == p_from){
+    for(int i=0; i<size; ++i) bin[i] = data[i];
+  }else{
+    if( my_rank_ == p_from)
+      BGNET_Send(0,data,size*sizeof(double),p_to);
+    if( my_rank_ == p_to)
+      BGNET_Recv(0,bin,size*sizeof(double),p_from);
+  }
+  BGNET_GlobalBarrier();
+}
+
+void Communicator::send_1to1(valarray<double>& bin,
+			     const valarray<double>& data, 
+			     int size,int p_to,int p_from,int tag) const{
+  if(p_to == p_from)    bin = data;
+  else send_1to1(&(bin[0]),&(const_cast<valarray<double>& >(data))[0],
+		 size,p_to,p_from,tag);
+}
+
+double Communicator::reduce_sum(double a) const{
+  double a_sum = 0.0;
+  BGNET_GlobalSum(&a_sum,&a);
+  return a_sum;
+}
+
+void Communicator::sync() const{ BGNET_GlobalBarrier();}
+
+void Communicator::broadcast(int size, int &data, int sender) const{
+  BGNET_BCast(&data,size,BGNET_COLLECTIVE_INT32,sender,BGNET_COMM_WORLD);
+}
+
+void Communicator::broadcast(int size, double &data, int sender) const{
+  BGNET_BCast(&data,size,BGNET_COLLECTIVE_DOUBLE,sender,BGNET_COMM_WORLD);
+}
+
+int Communicator::reduce_max(double& val,int& idx,int size) const{
+  /*! size is the maximum value of idx */
+  //  VaId vi = {val, my_rank_*size +idx};
+  //  VaId vo;
+  double vIn = val;
+  int idxIn = my_rank_*size +idx;
+  int idxOut;
+  BGNET_AllReduceLoc(&vIn,&idxIn,&val,&idxOut,1,BGNET_COLLECTIVE_DOUBLE,BGNET_COLLECTIVE_MAX,BGNET_COMM_WORLD);
+
+  idx = idxOut%size;
+  return idxOut/size;
+}
+
+int Communicator::reduce_min(double& val,int& idx,int size) const{
+  /*! size is the maximum value of idx */
+  //  VaId vi = {val, my_rank_*size +idx};
+  //  VaId vo;
+  double vIn = val;
+  int idxIn = my_rank_*size +idx;
+  int idxOut;
+  BGNET_AllReduceLoc(&vIn,&idxIn,&val,&idxOut,1,BGNET_COLLECTIVE_DOUBLE,BGNET_COLLECTIVE_MIN,BGNET_COMM_WORLD);
+
+  idx = idxOut%size;
+  return idxOut/size;
+}
+
+int Communicator::pprintf(const char* format ...) const{
+  va_list ap;
+  int ret = 0;
+
+  va_start(ap,format);
+  if(my_rank_==0)
+    ret = vfprintf( stdout, format, ap );
+  va_end(ap);
+
+  return ret;
+}
+
+
+#endif
