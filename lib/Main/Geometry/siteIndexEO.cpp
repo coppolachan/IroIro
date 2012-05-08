dbf3c79bf5a55205035d03ecaa6da6b72b9abe34
Guido Cossu <cossu@post.kek.jp>
2012-05-08 14:49:59 +0900

    [DEV-TEST] Debugged EvenOdd new site indexing

--- lib/Main/Geometry/siteIndexEO.cpp
+++ lib/Main/Geometry/siteIndexEO.cpp
@@ -79,13 +79,19 @@ int SiteIndex::m_z(int site) const{ return (site+Nvolh_)%Nvol_-Nxh_*Ny_;}
 int SiteIndex::m_t(int site) const{ return (site+Nvolh_)%Nvol_-Nxh_*Ny_*Nz_;}
 
 int SiteIndex::slice_x(int x,int n) const{
-  int parity = 2*n/(Ny_*Nz_*Nt_);
-  int idx = 2*n-parity*(Ny_*Nz_*Nt_);
+  int n1 = (x&1)*((Ny_*Nz_*Nt_)/2+n)%((Ny_*Nz_*Nt_))+(1-x&1)*n;
+  int parity = 2*n1/(Ny_*Nz_*Nt_);
+  int idx = 2*n1-parity*(Ny_*Nz_*Nt_);
   return idx*Nxh_+ ((x+parity+idx%(Nz_*Ny_)/Nz_+idx%Ny_+idx/(Ny_*Nz_)) & 1)*Nxh_+parity*Nvolh_+(x >> 1);}
 int SiteIndex::slice_y(int y,int n) const{
-  return (n/Nxh_)*Nxh_*Ny_+y*Nxh_+n%Nxh_;}
-int SiteIndex::slice_z(int z,int n) const{ return (n/(Nxh_*Ny_))*Nxh_*Ny_*Nz_+z*Nxh_*Ny_+n%(Nxh_*Ny_);}
-int SiteIndex::slice_t(int t,int n) const{ return t*NxNyNz_+n;}
+  int n1 = (y&1)*((Nx_*Nz_*Nt_)/2+n)%((Nx_*Nz_*Nt_))+(1-y&1)*n;
+  return (n1/Nxh_)*Nxh_*Ny_+y*Nxh_+n1%Nxh_;}
+int SiteIndex::slice_z(int z,int n) const{
+  int n1 = (z&1)*((Nx_*Ny_*Nt_)/2+n)%((Nx_*Ny_*Nt_))+(1-z&1)*n;
+  return (n1/(Nxh_*Ny_))*Nxh_*Ny_*Nz_+z*Nxh_*Ny_+n1%(Nxh_*Ny_);}
+int SiteIndex::slice_t(int t,int n) const{
+  int n1 = (t&1)*((Nx_*Ny_*Nz_)/2+n)%((Nx_*Ny_*Nz_))+(1-t&1)*n;
+  return (n1/(Nxh_*Ny_*Nz_))*Nxh_*Ny_*Nz_*Nt_+n1%(Nxh_*Ny_*Nz_)+t*(Nxh_*Ny_*Nz_);}
 
 int SiteIndex::slsize(int x,int dir)const{ return slsize_[dir];}
 
