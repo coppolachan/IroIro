0dd2c353ebe3f33c8d3ac31b42673ef35268336a
Jun-Ichi Noaki <noaki@post.kek.jp>
2012-07-20 13:39:10 +0900

    testerWilsonFlow.cpp added

---
+++ tests/testerWilsonFlow.cpp
@@ -0,0 +1,21 @@
+/*! @file testerWilsonFlow.cpp 
+ * @brief Main source code for testing the WilsonFlow classes
+ */
+
+#include "test_WilsonFlow.hpp"
+
+int main(int argc, char* argv[]){
+  
+  CREATE_RUN_TEST(Test_WilsonFlow);
+  CLEAR_TEST;
+  
+  return 0;
+}
+
+
+/*
+int main(int argc, char* argv[]){
+  int ret = TestEnv::StartRun<Test_WilsonFlow>(argc,argv);
+  return rret;
+}
+*/
