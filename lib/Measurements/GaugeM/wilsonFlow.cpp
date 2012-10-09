bb6dc29c5c27650190de0eaa2a3bbde83802c264
Jun-Ichi Noaki <noaki@post.kek.jp>
2012-10-10 05:16:52 +0900

    WilsonFlow is debugged.

--- lib/Measurements/GaugeM/wilsonFlow.cpp
+++ lib/Measurements/GaugeM/wilsonFlow.cpp
@@ -12,7 +12,7 @@ void WilsonFlow::update_U(const GaugeField& Z)const{
 
   for(int m=0; m<NDIM_; ++m){
     for(int site=0; site<Nvol_; ++site){
-      SUNmat au = exponential(mat(Z,site,m)*estep_,Nexp_);
+      SUNmat au = exponential(mat(Z,site,m)*estep_*g0q_,Nexp_);
       au *= mat(U_,site,m);
       U_.data.set(U_.format.islice(site,m),au.reunit().getva());
     }
@@ -20,28 +20,63 @@ void WilsonFlow::update_U(const GaugeField& Z)const{
 }
 
 void WilsonFlow::gradFlow()const{
-  //                                  W0 = U_
+                                   // W0 = U_
   GaugeField Z = Sg_->md_force();  // Z0
   Z *= 0.25;
-  update_U(Z);                     // W1 = exp(Z0/4)*W0 
+  update_U(Z);                     // U_= W1 = exp(ep*Z0/4)*W0 
 
-  Z *= -17.0/32.0;                    
-  Z += Sg_->md_force();            // +Z1
-  Z *= 8.0/9.0;
-  update_U(Z);                     // W2 = exp(8/9*Z1 -17/36*Z0)*W1
-
-  Z /= -0.75;                 
-  Z += Sg_->md_force();            // +Z2
-  Z *= 0.75;               
-  update_U(Z);                // V(t+e) = exp(3/4*Z2 -8/9*Z1 +17/36*Z0)*W2
-}
+  Z *= -17.0/8.0;
+  Z += Sg_->md_force();            // -17/32*Z0 +Z1 
+  Z *= 8.0/9.0;                    // -17/36*Z0 +8/9*Z1
+  update_U(Z);                     // U_= W2 = exp(ep*(-17/36*Z0 +8/9*Z1))*W1
   
+  Z *= -4.0/3.0;
+  Z += Sg_->md_force();            // 4/3*(17/36*Z0 -8/9*Z1) +Z2       
+  Z *= 0.75;                       // 17/36*Z0 -8/9*Z1 +3/4*Z2       
+  update_U(Z);              // V(t+e) = exp(ep*(17/36*Z0 -8/9*Z1 +3/4*Z2))*W2
+}
+
+/*
+void WilsonFlow::gradFlow()const{
+                                    // W0 = U_
+  GaugeField Z0 = Sg_->md_force();  // Z0
+  Z0 *= 0.5;
+  update_U(Z0);                     // U_= W1 = exp(ep*Z0/2)*W0 
+  Z0 *= 2.0;
+
+  GaugeField Z1 = Sg_->md_force();  // Z1
+  GaugeField Zt = Z1;
+  Zt -= Z0;
+  Zt *= 1.0-sqrt(2.0)*0.5;          // (1-1/sqrt(2))*(Z1-Z0)  
+  update_U(Zt);                     // U_= W2 = exp(ep*Z0/2)*W1 
+
+  GaugeField Z2 = Sg_->md_force();  // Z2
+  Z2 *= 1.0+sqrt(2.0)*0.5;          // (1+1/sqrt(2))*Z2
+  Zt = Z2;
+  Zt -= Z1;
+  Z0 *= -0.5*(sqrt(2.0)-1.0);
+  Zt += Z0;                        // (1+1/sqrt(2))*Z2 -Z1 +(1/2-1/sqrt(2))*Z0
+  Z0 *= -2.0*(sqrt(2.0)+1.0);       
+  update_U(Zt);// U_= W3= exp(ep*((1+1/sqrt(2))*Z2 -Z1 +(1/2-1/sqrt(2))*Z0))*W1 
+
+  Zt = Sg_->md_force();     // Z3       
+  Z2 *= -4.0;             
+  Z1 *= 2.0*(1.0 +sqrt(2.0)); 
+  Zt += Z2;                 // Z3 -4*c2*Z2 
+  Zt += Z1;                 // Z3 -4*c2*Z2 +(6-4*c1)*Z1
+  Zt += Z0;                 // Z3 -4*c2*Z2 +(6-4*c1)*Z1 +Z0
+  Zt /= 6.0;
+  update_U(Zt);          
+}
+*/
+
 std::vector<double> WilsonFlow::evolve()const{
 
-  std::vector<double> Eplaq(Nstep_);
+  std::vector<double> ttE(Nstep_);
   for(int t=0; t< Nstep_; ++t){
     gradFlow();
-     Eplaq[t] = 2.0/double(Lvol_)*Sg_->calc_H();
+    double tau = estep_*(t+1);
+    ttE[t] = tau*tau*fabs(g0q_)*Sg_->calc_H()/double(Lvol_);
   }
-  return Eplaq;
+  return ttE;
 }
--- lib/Measurements/GaugeM/wilsonFlow.hpp
+++ lib/Measurements/GaugeM/wilsonFlow.hpp
@@ -8,6 +8,8 @@
 #include "include/commonPrms.h"
 #include "include/common_fields.hpp"
 #include "include/pugi_interface.h"
+#include "include/macros.hpp"
+
 #include "Action/action_gauge_wilson.hpp"
 
 class WilsonFlow{
@@ -16,6 +18,7 @@ private:
   int Nexp_;
   int Nstep_;
   double estep_;
+  double g0q_;
   mutable GaugeField U_; // internal buffer, initialized by input U
   ActionGaugeWilson* Sg_;
 
@@ -24,17 +27,23 @@ private:
 public:
   WilsonFlow(XML::node node,const GaugeField& U)
     :U_(U),
-     Sg_(new ActionGaugeWilson(NC_,&U_)),
+     Sg_(NULL),
      Nvol_(CommonPrms::instance()->Nvol()),
      Lvol_(CommonPrms::instance()->Lvol()){
+    double beta;
+    XML::read(node,"beta",beta,MANDATORY);
+    Sg_= new ActionGaugeWilson(beta,&U_);
+    g0q_= -1.0/beta*2.0*NC_;
+
     XML::read(node,"Nexp",Nexp_,MANDATORY);
     XML::read(node,"Nstep",Nstep_,MANDATORY);
     XML::read(node,"estep",estep_,MANDATORY);
+    //estep_*=-1.0;
   }
 
-  WilsonFlow(int Nexp,int Nstep,double estep,const GaugeField& U)
+  WilsonFlow(double beta,int Nexp,int Nstep,double estep,const GaugeField& U)
     :Nexp_(Nexp),Nstep_(Nstep),estep_(estep),U_(U),
-     Sg_(new ActionGaugeWilson(NC_,&U_)),
+     Sg_(new ActionGaugeWilson(beta,&U_)),
      Nvol_(CommonPrms::instance()->Nvol()),
      Lvol_(CommonPrms::instance()->Lvol()){}
   
