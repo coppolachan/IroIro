4fa033da435c13f622da17b2c4fb7fa9bce5cc8a
Jun Noaki <noaki@post.kek.jp>
2014-09-10 09:53:48 +0900

    * small problem in compilation of tests/ solved * Test_GWrelEigen modified (by A.Tomiya)

--- tests/test_GWrelEigen.cpp
+++ tests/test_GWrelEigen.cpp
@@ -24,12 +24,13 @@ int Test_GWrelEigen::run(){
     double mass;
     XML::read(gw_node,"mass", mass,MANDATORY);
     Dirac_Wilson Dw(mass,&(input_.gconf->data));
+    CCIO::cout<<"CP:Pairling:mass="<< mass <<endl;
 
     for(int i=0; i<Neig; ++i){
       double sign = evals_[i]/sqrt(evals_[i]*evals_[i]);
       double lmd = 0.0;
       if(abs(evals_[i])<0.001)
-	CCIO::cout<<"CP:co:lmd=0 beacause of near 0-mode evals["<<i<<"]="
+	CCIO::cout<<"CP:lmd=0 beacause of near 0-mode evals["<<i<<"]="
 		  <<evals_[i]<<endl;
       else
 	lmd = sign*sqrt(abs((evals_[i]*evals_[i] -mass*mass)/(1.0 -mass*mass)));
@@ -41,14 +42,14 @@ int Test_GWrelEigen::run(){
 
       double beta = mass/(evals_[i]+(1.0 -mass)*lmd);
 
-      CCIO::cout<<"CP:co:Pairling: "<< i <<" sign "<< sign <<endl;
-      CCIO::cout<<"CP:co:Pairling: "<< i <<" beta "<< beta <<endl;
-      CCIO::cout<<"CP:co:Pairling: "<< i <<" massless_eval "<< lmd <<endl;
+      CCIO::cout<<"CP:Pairling: "<< i <<" sign "<< sign <<endl;
+      CCIO::cout<<"CP:Pairling: "<< i <<" beta "<< beta <<endl;
+      CCIO::cout<<"CP:Pairling: "<< i <<" massless_eval "<< lmd <<endl;
 
-      double fii = 2.0*beta*fmij -1.0 +2.0*beta*lmd -beta*beta*(1.0 -2.0*lmd*lmd);
-      fii /= -fmij*(1.0 +2.0*beta*lmd+beta*beta)
+      double fii = -fmij*(1.0 +2.0*beta*lmd+beta*beta)
 	      +2.0*beta -2.0*beta*lmd*lmd +2.0*beta*beta*lmd
               -2.0*beta*beta*lmd*lmd*lmd;
+      fii /= 2.0*beta*fmij -1.0 +2.0*beta*lmd -beta*beta*(1.0 -2.0*lmd*lmd);
 
       double gii = phi*evecs_[i]-(evals_[i]*evals_[i]+mass)
 	/(evals_[i]*(1.0 +mass));
@@ -56,11 +57,12 @@ int Test_GWrelEigen::run(){
       CCIO::cout<<"CP:massive:Pairling:fmii: "<< i <<" "<< fmij <<endl;
       CCIO::cout<<"CP:massless:Pairling:fii: "<< i <<" "<< fii <<endl;
       CCIO::cout<<"CP:gii: "<< i <<" "<< gii <<endl;
-      CCIO::cout<<"CP:massive_eval: "<< i <<evals_[i] <<endl;
+      CCIO::cout<<"CP:massive_eval: "<< i << " " << evals_[i] <<endl;
     }
   }catch(const char* error){
     CCIO::cout<<error<<"\n";
   }
+    CCIO::cout<<"CP:Pairling:END"<<endl;
   return 0;
 }
 
